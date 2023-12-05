b76008539346796f014e456f7a1b8f9ecdc97a6c
Yakir <yakirhuri21@gmail.com>
2023-12-05 13:25:30 +0200

    wip

--- src/peyk_auto_reboot_node.py
+++ src/peyk_auto_reboot_node.py
@@ -6,14 +6,14 @@ import time
 import os
 
 class ImageSubscriberNode:
-    def __init__(self, max_messages, time_interval):
+    def __init__(self, max_messages, time_interval, image_topic_name):
         self.max_messages = max_messages
         self.time_interval = time_interval
         self.received_messages = 0
         self.start_time = time.time()
 
         # Subscribe to the image topic
-        rospy.Subscriber("/usb_cam/image_raw", Image, self.image_callback)
+        rospy.Subscriber(image_topic_name, Image, self.image_callback)
 
         # Create a timer to check the condition periodically
         self.timer = rospy.Timer(rospy.Duration(1), self.timer_callback)
@@ -37,17 +37,20 @@ class ImageSubscriberNode:
         elif time.time() - self.start_time >= self.time_interval:
             rospy.loginfo("Did not receive {} messages in {} seconds. Rebooting the computer.".format(
                 self.max_messages, self.time_interval))
-            os.system("sudo reboot")
+            #os.system("sudo reboot")
             #self.timer.shutdown()  # Stop the timer
 def main():
     rospy.init_node('peyk_auto_reboot_node', anonymous=True)
 
     # Specify the maximum number of messages and time interval
     max_messages = 10
-    time_interval = 30  # seconds
+    time_interval = rospy.get_param('time_interval_seconds', 30)
+
+    image_topic_name = rospy.get_param('image_topic_name', '/merged_image')
+
 
     # Create an instance of the ImageSubscriberNode class
-    image_subscriber_node = ImageSubscriberNode(max_messages, time_interval)
+    image_subscriber_node = ImageSubscriberNode(max_messages, time_interval, image_topic_name)
 
     # Spin the ROS node
     rospy.spin()
